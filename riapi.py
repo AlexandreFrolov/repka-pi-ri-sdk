0e4eb67ba67ca97ab628b20415e578f9c790b0cb
AlexandreFrolov <alexandre@frolov.pp.ru>
2023-07-17 15:07:18 +0300

    Add Exceptions

--- riapi.py
+++ riapi.py
@@ -4,7 +4,11 @@ from ctypes import *
 
 class RiApi:
     def __init__(self, is_async: c_bool):
-        self.lib = cdll.LoadLibrary("C:\Windows\system32\librisdk.dll")        
+        try:
+            self.lib = cdll.LoadLibrary("C:\Windows\system32\librisdk.dll")
+        except OSError as e:
+            raise Exception("Failed to load DLL: " + str(e))
+
         self.errTextC = create_string_buffer(1000)  # Текст ошибки. C type: char*
         self.i2c = c_int()
         self.pwm = c_int()
@@ -21,35 +25,27 @@ class RiApi:
         self.lib.RI_SDK_CreateBasic.argtypes = [POINTER(c_int), c_char_p]
         self.lib.RI_SDK_CreateModelComponent.argtypes = [c_char_p, c_char_p, c_char_p, POINTER(c_int), c_char_p]
         self.lib.RI_SDK_LinkPWMToController.argtypes = [c_int, c_int, c_uint8, c_char_p]
-
         descriptor = c_int()
 
         errCode = self.lib.RI_SDK_InitSDK(1, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
+            raise Exception(f"init: RI_SDK_InitSDK failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
         errCode = self.lib.RI_SDK_CreateBasic(descriptor, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
-#        print("descriptor: ", descriptor.value)
-
+            raise Exception(f"init: RI_SDK_CreateBasic failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
         errCode = self.lib.RI_SDK_CreateModelComponent("connector".encode(), "pwm".encode(), "pca9685".encode(), self.pwm, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"init: RI_SDK_CreateModelComponent failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
         errCode = self.lib.RI_SDK_CreateModelComponent("connector".encode(), "i2c_adapter".encode(), "ch341".encode(), self.i2c, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"init: RI_SDK_CreateModelComponent failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
         errCode = self.lib.RI_SDK_LinkPWMToController(self.pwm, self.i2c, c_uint8(0x40), self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"init: RI_SDK_LinkPWMToController failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def add_servo(self, servo, servo_type, channel):
         self.lib.RI_SDK_CreateModelComponent.argtypes = [c_char_p, c_char_p, c_char_p, POINTER(c_int), c_char_p]
@@ -57,79 +53,124 @@ class RiApi:
 
         errCode = self.lib.RI_SDK_CreateModelComponent("executor".encode(), "servodrive".encode(), servo_type.encode(), servo, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"add_servo: RI_SDK_CreateModelComponent failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
         errCode = self.lib.RI_SDK_LinkServodriveToController(servo, self.pwm, channel, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"add_servo: RI_SDK_LinkServodriveToController failed with error code {errCode}: {self.errTextC.raw.decode()}")        
+
+
+
+    def add_custom_servo(self, servo, MaxDt, MinDt, MaxSpeed, RangeAngle):
+        self.lib.RI_SDK_CreateDeviceComponent.argtypes = [c_char_p, c_char_p,  POINTER(c_int), c_char_p]
+        self.lib.RI_SDK_exec_ServoDrive_CustomDeviceInit.argtypes = [c_int, c_int, c_int, c_int, c_int, c_char_p]
+
+        errCode = self.lib.RI_SDK_CreateDeviceComponent("executor".encode(), "servodrive".encode(),  servo, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"add_custom_servo: RI_SDK_CreateDeviceComponent failed with error code {errCode}: {self.errTextC.raw.decode()}")
+
+        errCode = self.lib.RI_SDK_exec_ServoDrive_CustomDeviceInit(servo, MaxDt, MinDt, MaxSpeed, RangeAngle, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"add_custom_servo: RI_SDK_exec_ServoDrive_CustomDeviceInit failed with error code {errCode}: {self.errTextC.raw.decode()}")
+
+
+    def rotate(self, servo, direction, speed):
+        self.lib.RI_SDK_exec_ServoDrive_Rotate.argtypes = [c_int, c_int, c_int, c_bool, c_char_p]
+
+        errCode = self.lib.RI_SDK_exec_ServoDrive_Rotate(servo, direction, speed, self.is_async, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"rotate: RI_SDK_exec_ServoDrive_Rotate failed with error code {errCode}: {self.errTextC.raw.decode()}")
+
+    def rotate_min_step(self, servo, direction, speed):
+        self.lib.RI_SDK_exec_ServoDrive_MinStepRotate.argtypes = [c_int, c_int, c_int, c_bool, c_char_p]
+
+        errCode = self.lib.RI_SDK_exec_ServoDrive_MinStepRotate(servo, direction, speed, self.is_async, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"rotate_min_step: RI_SDK_exec_ServoDrive_MinStepRotate failed with error code {errCode}: {self.errTextC.raw.decode()}")
+
+    def set_middle(self, servo):
+        self.lib.RI_SDK_exec_ServoDrive_SetPositionToMidWorkingRange.argtypes = [c_int, c_char_p]
+
+        errCode = self.lib.RI_SDK_exec_ServoDrive_SetPositionToMidWorkingRange(servo, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"turn: RI_SDK_exec_ServoDrive_Rotate failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def turn_by_pulse(self, servo, dt):
         errCode = self.lib.RI_SDK_exec_ServoDrive_TurnByPulse(servo, dt, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
+            raise Exception(f"turn_by_pulse: RI_SDK_exec_ServoDrive_TurnByPulse failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def turn_by_angle(self, servo, angle, speed):
         errCode = self.lib.RI_SDK_exec_ServoDrive_Turn(servo, angle, speed, self.is_async, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
+            raise Exception(f"turn_by_angle: RI_SDK_exec_ServoDrive_Turn failed with error code {errCode}: {self.errTextC.raw.decode()}")
+
+    def turn_by_duty(self, servo, steps):
+        self.lib.RI_SDK_exec_ServoDrive_TurnByDutyCycle.argtypes = [c_int, c_int, c_char_p]
+        errCode = self.lib.RI_SDK_exec_ServoDrive_TurnByDutyCycle(servo, steps, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"turn_by_duty: RI_SDK_exec_ServoDrive_TurnByDutyCycle failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def get_angle(self, servo):
         self.lib.RI_SDK_exec_ServoDrive_GetCurrentAngle.argtypes = [c_int, POINTER(c_int), c_char_p]
         angle = c_int()
         errCode = self.lib.RI_SDK_exec_ServoDrive_GetCurrentAngle(servo, angle, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"get_angle: RI_SDK_exec_ServoDrive_GetCurrentAngle failed with error code {errCode}: {self.errTextC.raw.decode()}")
         return(angle.value) 
 
     def cleanup_servo(self, servo):
         self.lib.RI_SDK_DestroyComponent.argtypes = [c_int, c_char_p]
         errCode = self.lib.RI_SDK_DestroyComponent(servo, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
+            raise Exception(f"cleanup_servo: RI_SDK_DestroyComponent failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
-    def add_led(self, led):
+    def add_led(self, led, r, g, b):
         self.lib.RI_SDK_CreateModelComponent.argtypes = [c_char_p, c_char_p, c_char_p, POINTER(c_int), c_char_p]
         self.lib.RI_SDK_LinkLedToController.argtypes = [c_int, c_int, c_int, c_int, c_int, c_char_p]
         errCode = self.lib.RI_SDK_CreateModelComponent("executor".encode(), "led".encode(), "ky016".encode(), led, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"add_led: RI_SDK_CreateModelComponent failed with error code {errCode}: {self.errTextC.raw.decode()}")
     #    errCode = self.lib.RI_SDK_LinkLedToController(led, self.pwm, 15, 14, 13, self.errTextC)
-        errCode = self.lib.RI_SDK_LinkLedToController(led, self.pwm, 14, 15, 13, self.errTextC)
+        errCode = self.lib.RI_SDK_LinkLedToController(led, self.pwm, r, g, b, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
+            raise Exception(f"add_led: RI_SDK_LinkLedToController failed with error code {errCode}: {self.errTextC.raw.decode()}")
+
+    def stop_led(self, led):
+        self.lib.RI_SDK_exec_RGB_LED_Stop.argtypes = [c_int, c_char_p]
+        errCode = self.lib.RI_SDK_exec_RGB_LED_Stop(led, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"stop_led: RI_SDK_exec_RGB_LED_Stop failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def cleanup_led(self, led):
         self.lib.RI_SDK_DestroyComponent.argtypes = [c_int, c_char_p]
         errCode = self.lib.RI_SDK_DestroyComponent(led, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"cleanup_led: RI_SDK_DestroyComponent failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def led_pulse(self, led, r, g, b, duration):
         self.lib.RI_SDK_exec_RGB_LED_SinglePulse.argtypes = [c_int, c_int, c_int, c_int, c_int, c_bool, c_char_p]
         errCode = self.lib.RI_SDK_exec_RGB_LED_SinglePulse(led, r, g, b, duration, self.is_async, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"led_pulse: RI_SDK_exec_RGB_LED_SinglePulse failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def led_pulse_pause(self, led, r, g, b, duration, pause, limit):
         self.lib.RI_SDK_exec_RGB_LED_FlashingWithPause.argtypes = [c_int, c_int, c_int, c_int, c_int, c_int, c_int, c_bool, c_char_p]
         errCode = self.lib.RI_SDK_exec_RGB_LED_FlashingWithPause(led, r, g, b, duration, pause, limit, self.is_async, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
+            raise Exception(f"led_pulse_pause: RI_SDK_exec_RGB_LED_FlashingWithPause failed with error code {errCode}: {self.errTextC.raw.decode()}")
+
+    def led_pulse_frequency(self, led, r, g, b, frequency, limit):
+        self.lib.RI_SDK_exec_RGB_LED_FlashingWithFrequency.argtypes = [c_int, c_int, c_int, c_int, c_int, c_int, c_bool, c_char_p]
+        errCode = self.lib.RI_SDK_exec_RGB_LED_FlashingWithFrequency(led, r, g, b, frequency, limit, self.is_async, self.errTextC)
+        if errCode != 0:
+            raise Exception(f"led_pulse_pause: RI_SDK_exec_RGB_LED_FlashingWithFrequency failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def led_flicker(self, led, r, g, b, duration, limit):
         self.lib.RI_SDK_exec_RGB_LED_Flicker.argtypes = [c_int, c_int, c_int, c_int, c_int, c_int, c_bool, c_char_p]
         errCode = self.lib.RI_SDK_exec_RGB_LED_Flicker(led, r, g, b, duration, limit, self.is_async, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)  
+            raise Exception(f"led_flicker: RI_SDK_exec_RGB_LED_Flicker failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
     def cleanup_final(self):
         self.lib.RI_SDK_sigmod_PWM_ResetAll.argtypes = [c_int, c_char_p]
@@ -137,15 +178,12 @@ class RiApi:
         self.lib.RI_SDK_DestroySDK.argtypes = [c_bool, c_char_p]
         errCode = self.lib.RI_SDK_sigmod_PWM_ResetAll(self.pwm, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
+            raise Exception(f"cleanup_final: RI_SDK_sigmod_PWM_ResetAll failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
         errCode = self.lib.RI_SDK_DestroyComponent(self.i2c, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2)
+            raise Exception(f"cleanup_final: RI_SDK_DestroyComponent failed with error code {errCode}: {self.errTextC.raw.decode()}")
 
         errCode = self.lib.RI_SDK_DestroySDK(True, self.errTextC)
         if errCode != 0:
-            print(errCode, self.errTextC.raw.decode())
-            sys.exit(2) 
+            raise Exception(f"cleanup_final: RI_SDK_DestroySDK failed with error code {errCode}: {self.errTextC.raw.decode()}")
--- riapi_test.py
+++ riapi_test.py
@@ -4,79 +4,146 @@ import time
 from ctypes import *
 from riapi import RiApi
 
-
 def main():
+    try:
+        servo_1 = c_int() 
+        servo_4 = c_int() 
+        servo_5 = c_int() 
+        led = c_int()
 
-    servo_1 = c_int() 
-    servo_4 = c_int() 
-    servo_5 = c_int() 
-    led = c_int()
+        api = RiApi(c_bool(False))
+    #    api = RiApi(c_bool(True))
+        api.init()
 
-#    api = RiApi(c_bool(False))
-    api = RiApi(c_bool(True))
-    api.init()
+        api.add_led(led, 14, 15, 13)
+#        api.async_off()
 
-    api.add_led(led)
+        # api.led_flicker(led, 255, 0, 0, 500, 5)
+        # api.led_flicker(led, 0, 255, 0, 500, 5)
+        # api.led_flicker(led, 0, 0, 255, 500, 5)
 
-    api.async_off()
+#        time.sleep(1) 
 
-    api.led_flicker(led, 255, 0, 0, 500, 5)
+        # api.led_pulse(led, 255, 0, 0, 500)
+        # api.led_pulse(led, 0, 255, 0, 500)
+        # api.led_pulse(led, 0, 0, 255, 500)
 
-    api.led_pulse(led, 255, 0, 0, 500)
-    api.led_pulse(led, 0, 255, 0, 500)
-    api.led_pulse(led, 0, 0, 255, 500)
+        # api.led_pulse_pause(led, 255, 0, 0, 1000, 200, 3)
+        # api.led_pulse_pause(led, 0, 255, 0, 1000, 200, 3)
+        # api.led_pulse_pause(led, 0, 0, 255, 1000, 200, 3)
 
-    api.led_pulse_pause(led, 255, 0, 0, 1000, 200, 3)
-    api.led_pulse_pause(led, 0, 255, 0, 1000, 200, 3)
-    api.led_pulse_pause(led, 0, 0, 255, 1000, 200, 3)
-
-    api.async_on()
-
-    api.add_servo(servo_1, "mg90s", 0)
-    api.add_servo(servo_4, "mg90s", 4)
-    api.add_servo(servo_5, "mg90s", 5)
-
-
-    api.turn_by_pulse(servo_1, 1600)
-    time.sleep(1) 
-
-    api.turn_by_angle(servo_1, 90, 200)
-    time.sleep(1) 
-
-    api.turn_by_angle(servo_1, -180, 300)
-    time.sleep(1) 
+        # api.led_pulse_frequency(led, 255, 0, 0, 10, 10)
+        # api.led_pulse_frequency(led, 0, 255, 0, 20, 10)
+        # api.led_pulse_frequency(led, 0, 0, 255, 30, 10)
 
 
-    api.turn_by_pulse(servo_4, 1600)
-    time.sleep(1) 
 
-    api.turn_by_angle(servo_4, 90, 200)
-    time.sleep(1) 
+#        api.async_on()
 
-    api.turn_by_angle(servo_4, -180, 300)
-    time.sleep(1) 
+        api.add_servo(servo_1, "mg90s", 0)
+#        api.add_custom_servo(servo_1, 2165, 380, 100, 2000)
+
+        api.add_servo(servo_4, "mg90s", 4)
+        api.add_servo(servo_5, "mg90s", 5)
 
 
-    api.turn_by_pulse(servo_5, 1600)
-    time.sleep(1) 
+        # MG90S
+        # api.turn_by_pulse(servo_1, 365) # 348 mc на осциллографе
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+        # api.turn_by_pulse(servo_1, 1260) # 1210 mc на осциллографе
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+        # api.turn_by_pulse(servo_1, 2160) # 2080 mc на осциллографе
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
 
-    api.turn_by_angle(servo_5, 90, 200)
-    time.sleep(1) 
 
-    api.turn_by_angle(servo_5, -180, 300)
-    time.sleep(1) 
 
-    print("servo_1 angle: " + str(api.get_angle(servo_1)))
-    print("servo_4 angle: " + str(api.get_angle(servo_4)))
-    print("servo_5 angle: " + str(api.get_angle(servo_5)))
+        api.set_middle(servo_1)
+        time.sleep(2) 
+        print("servo_1 angle: " + str(api.get_angle(servo_1)))
 
-    api.cleanup_servo(servo_1)
-    api.cleanup_servo(servo_4)
-    api.cleanup_servo(servo_5)
-    api.cleanup_led(led)
-    api.cleanup_final()
+        #api.rotate(servo_1, 0, 100)
+        #time.sleep(2) 
+        #print("servo_1 angle: " + str(api.get_angle(servo_1)))
 
-    print("Success")
+        # api.rotate(servo_1, 1, 100)
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+
+
+        api.rotate_min_step(servo_1, 1, 100)
+        time.sleep(2) 
+        print("servo_1 angle: " + str(api.get_angle(servo_1)))
+        api.rotate_min_step(servo_1, 0, 100)
+        time.sleep(2) 
+        print("servo_1 angle: " + str(api.get_angle(servo_1)))
+
+
+        # api.turn_by_duty(servo_1, 75)
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+
+        # api.turn_by_duty(servo_1, 300)
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+
+        # api.turn_by_duty(servo_1, 540)
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+
+
+        # SG90
+        # api.turn_by_pulse(servo_1, 380)
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+        # api.turn_by_pulse(servo_1, 1260)
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+        # api.turn_by_pulse(servo_1, 2165)
+        # time.sleep(2) 
+        # print("servo_1 angle: " + str(api.get_angle(servo_1)))
+
+
+
+        # api.turn_by_angle(servo_1, 90, 200)
+        # time.sleep(1) 
+
+        # api.turn_by_angle(servo_1, -180, 300)
+        # time.sleep(1) 
+
+
+        # api.turn_by_pulse(servo_4, 1600)
+        # time.sleep(1) 
+        # api.turn_by_angle(servo_4, 90, 200)
+        # time.sleep(1) 
+        # api.turn_by_angle(servo_4, -180, 300)
+        # time.sleep(1) 
+
+
+        # api.turn_by_pulse(servo_5, 1600)
+        # time.sleep(1) 
+        # api.turn_by_angle(servo_5, 90, 200)
+        # time.sleep(1) 
+        # api.turn_by_angle(servo_5, -180, 300)
+        # time.sleep(1) 
+
+#        print("servo_1 angle: " + str(api.get_angle(servo_1)))
+        # print("servo_4 angle: " + str(api.get_angle(servo_4)))
+        # print("servo_5 angle: " + str(api.get_angle(servo_5)))
+
+        api.cleanup_servo(servo_1)
+        api.cleanup_servo(servo_4)
+        api.cleanup_servo(servo_5)
+        api.cleanup_led(led)
+        api.cleanup_final()
+
+        print("Success")
+
+    except Exception as e:
+        print("Class RiApi Error:", str(e))
+        sys.exit(2)
 
 if __name__ == "__main__":
     main()
